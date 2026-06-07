23b30bac017065a09af230a5f3f142576b6502e0
Farha Naaz <farha.naaz.2004.222@gmail.com>
2026-06-07 20:39:05 +0530

    Refactor handleContact to save contact submissions
    Refactored handleContact method to save contact data to the repository instead of just printing it.

--- src/main/java/com/task1/demo/controller/ContactController.java
+++ src/main/java/com/task1/demo/controller/ContactController.java
@@ -1,24 +1,57 @@
 package com.task1.demo.controller;
 
+import com.task1.demo.entity.Contact;
+import com.task1.demo.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ContactController {
-
-
+@Autowired
+    private ContactRepository repo;
 
         @PostMapping("/contact")
-        public String handleContact(@RequestParam String name,
-                                    @RequestParam String email,
-                                    @RequestParam String message) {
-            System.out.println("New Contact Form Submission");
-            System.out.println("Name: " + name);
-            System.out.println("Email: " + email);
-            System.out.println("Message: " + message);
 
-            return "Form submitted successfull";
+        public String handleContact(
+                @RequestParam String name,
+                @RequestParam String email,
+                @RequestParam String message) {
+
+            Contact contact = new Contact();
+
+            contact.setName(name);
+            contact.setEmail(email);
+            contact.setMessage(message);
+
+            repo.save(contact);
+
+            return "Form submitted successfully";
         }
+//        public String handleContact(@RequestParam String name,
+//                                    @RequestParam String email,
+//                                    @RequestParam String message) {
+//            System.out.println("New Contact Form Submission");
+//            System.out.println("Name: " + name);
+//            System.out.println("Email: " + email);
+//            System.out.println("Message: " + message);
+//
+//            return "Form submitted successfull";
+//
+//        }
+
+
+    @PostMapping("/submit")
+    public Contact saveContact(@RequestBody Contact contact)
+    {
+        return repo.save(contact);
+    }
+
+    @GetMapping("/contacts")
+    public List<Contact> getALlContact()
+    {
+        return repo.findAll();
+    }
     }
